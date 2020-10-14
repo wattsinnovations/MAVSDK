23d3054ee55db70a71ff18ebba62462a0a1023ed
Jacob Dahl <dahl.jakejacob@gmail.com>
2020-10-13 16:32:42 -0800

    added a helper script to build for armv6 (rpi zero w)

---
+++ build_armv6.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+dockcross-linux-armv6 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install -DDEPS_INSTALL_PATH=install -DBUILD_SHARED_LIBS=OFF -Bbuild/linux-armv6 -S.
+dockcross-linux-armv6 cmake --build build/linux-armv6 -j 8 --target install
