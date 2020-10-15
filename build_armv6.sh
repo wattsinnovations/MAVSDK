5adb6b651d766a8c49cff92ca3af3ff0feb7eb90
Jacob Dahl <dahl.jakejacob@gmail.com>
2020-10-14 17:02:56 -0800

    fixed for mavlink from QGC4.0

--- build_armv6.sh
+++ build_armv6.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-dockcross-linux-armv6 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install -DDEPS_INSTALL_PATH=install -DBUILD_SHARED_LIBS=OFF -Bbuild/linux-armv6 -S.
+dockcross-linux-armv6 cmake -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install -DDEPS_INSTALL_PATH=install -DBUILD_SHARED_LIBS=OFF -Bbuild/linux-armv6 -S.
 dockcross-linux-armv6 cmake --build build/linux-armv6 -j 8 --target install
