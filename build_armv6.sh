bbbd10b8d57441a9139a91171c27cba2fad61693
Jacob Dahl <dahl.jakejacob@gmail.com>
2020-10-17 23:51:28 -0800

    updated mavlink, trying to figure out libressl and curl linking with rt and pthread

--- build_armv6.sh
+++ build_armv6.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-dockcross-linux-armv6 cmake -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DDEPS_INSTALL_PATH=install -DBUILD_SHARED_LIBS=OFF -Bbuild/linux-armv6 -S.
-dockcross-linux-armv6 cmake --build build/linux-armv6 -j 8 --target install
+raspberry-armv6-container cmake -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DDEPS_INSTALL_PATH=install -DBUILD_SHARED_LIBS=OFF -Bbuild/linux-armv6 -S.
+raspberry-armv6-container cmake --build build/linux-armv6 -j 10 --target install
