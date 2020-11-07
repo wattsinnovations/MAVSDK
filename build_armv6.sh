e786ab8821ba635ee95921dd82f270fb68173de1
Jacob Dahl <dahl.jakejacob@gmail.com>
2020-11-06 17:05:39 -0900

    remove build/ and install/ at start of build script

--- build_armv6.sh
+++ build_armv6.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+rm -rf build/
+rm -rf install/
 raspberry-armv6-container cmake -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -DDEPS_INSTALL_PATH=install -DBUILD_SHARED_LIBS=OFF -Bbuild/linux-armv6 -S.
 raspberry-armv6-container cmake --build build/linux-armv6 -j 10 --target install
